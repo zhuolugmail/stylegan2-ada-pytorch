33cfda403ddc378361c25d2c664193680cd6f566
Zhuo Lu <zhuolu@gmail.com>
2022-02-18 20:54:36 -0600

    Updated scripts and instructions.

--- clouds/jarvislabsai/train.sh
+++ clouds/jarvislabsai/train.sh
@@ -1,10 +1,10 @@
-. ../sg/bin/activate
+# . ../sg/bin/activate
 
-python train.py --data=../processed --outdir=../out_penguin \
-	       --gpus=1 --kimg=16 --snap=1 --metrics=None --cfg=11gb-gpu \
-	       --resume=../out_penguin/00000-processed-11gb-gpu-kimg16-resumeffhq512/network-snapshot-000016.pkl 
+python train.py --data=../processed --outdir=../out \
+	       --gpus=1 --kimg=100 --snap=5 --metrics=None --cfg=11gb-gpu \
+	       --resume= 
 
-deactivate
+# deactivate
 
 sleep 1
 
